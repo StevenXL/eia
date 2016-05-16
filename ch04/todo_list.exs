cbb6ed09a9b6fc64f1b16bb441ac5bc956096d3c
Steven Leiva <leiva.steven@gmail.com>
2016-05-15 23:26:36 -0400

    Add feature to update an entry in a TodoList.

--- ch04/todo_list.exs
+++ ch04/todo_list.exs
@@ -1,11 +1,32 @@
 defmodule TodoList do
-  def new, do: Map.new
+  defstruct auto_id: 1, entries: Map.new
 
-  def add_entry(todo_list, entry = %{date: date, title: _}) do
-    Map.update(todo_list, date, [entry], &([entry| &1]))
+  def new, do: %TodoList{}
+
+  def add_entry(todo_list = %TodoList{auto_id: auto_id, entries: entries}, entry = %{}) do
+    entry = Map.put(entry, :id, auto_id)
+    new_entries  = Map.put(entries, auto_id, entry)
+    %{todo_list | entries: new_entries, auto_id: (auto_id + 1)}
   end
 
-  def entries(todo_list, date = {_, _, _}) do
-    Map.get(todo_list, date, nil)
+  def entries(%TodoList{entries: entries}, date = {_, _, _}) do
+    entries
+    |> Stream.filter(fn({_, entry}) ->
+      entry.date == date
+    end)
+    |> Enum.map(fn({_, entry}) ->
+      entry
+    end)
+  end
+
+  def update_entry(todo_list = %TodoList{entries: entries}, entry_id, update_fun) do
+    case entries[entry_id] do
+      nil -> todo_list
+
+      entry ->
+        new_entry = update_fun.(entry)
+        new_entries = %{entries | entry_id => new_entry}
+        %{todo_list | entries: new_entries}
+    end
   end
 end
