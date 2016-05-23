5dd0bc29dc2a799396b8e40c83508cb37de81d58
Steven Leiva <leiva.steven@gmail.com>
2016-05-23 08:42:21 -0400

    Pass an argument (the name of the server) to the Todo.Server.start function from the Todo.Cache server.

--- ch07/todo/lib/todo/cache.ex
+++ ch07/todo/lib/todo/cache.ex
@@ -19,7 +19,7 @@ defmodule Todo.Cache do
   def handle_call({:todo_pid, name}, _from, state) do
     case Map.get(state, name, nil) do
       nil ->
-        {:ok, todo_pid} = Todo.Server.start()
+        {:ok, todo_pid} = Todo.Server.start(name)
         new_state = Map.put(state, name, todo_pid)
         {:reply, todo_pid, new_state}
       todo_pid -> {:reply, todo_pid, state}
