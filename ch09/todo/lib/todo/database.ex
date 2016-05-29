106f034b0b16bfcd4558ea13c5e36410db98c751
Steven Leiva <leiva.steven@gmail.com>
2016-05-29 10:15:23 -0400

    Refactor Todo.Database to no longer be a Genserver. This module simply serves as an interface between the database part of the system and the rest of the system. Notice that its start_link function delegates to Todo.PoolSupervisor.

--- ch09/todo/lib/todo/database.ex
+++ ch09/todo/lib/todo/database.ex
@@ -1,9 +1,8 @@
 defmodule Todo.Database do
-  use GenServer
+  @pool_size 3
 
-  # Client API #
   def start_link(folder) do
-    GenServer.start_link(__MODULE__, folder, name: :db)
+    Todo.PoolSupervisor.start_link(folder, @pool_size) # delegate to the supervisor
   end
 
   def store(key, data) do
@@ -18,30 +17,9 @@ defmodule Todo.Database do
     |> Todo.DatabaseWorker.retrieve(key)
   end
 
-  # Server API #
-
-  def init(folder) when is_binary(folder) do
-    IO.puts "Initializing the Todo.Database"
-
-    {:ok, start_workers(folder)}
-  end
-
-  def handle_call({:get_worker, key}, _from, workers) do
-    worker = Enum.at(workers, :erlang.phash2(key, 3))
-    {:reply, worker, workers}
-  end
-
   # Helper Functions #
 
   defp get_worker(key) do
-    GenServer.call(:db, {:get_worker, key})
-  end
-
-  defp start_workers(folder) when is_binary(folder) do
-    1..3
-    |> Enum.map(fn(idx) ->
-      {:ok, pid} = Todo.DatabaseWorker.start_link(folder, idx)
-      pid
-    end)
+    :erlang.phash2(key, @pool_size) + 1
   end
 end
